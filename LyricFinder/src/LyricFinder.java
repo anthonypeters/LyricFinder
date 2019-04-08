1fd1efa71e9b143582ee9d6f5c60ef84712e3ef6
anthonypeters <anthony.peters68@yahoo.com>
2019-04-08 15:23:47 -0500

    Update LyricFinder.java

--- LyricFinder/src/LyricFinder.java
+++ LyricFinder/src/LyricFinder.java
@@ -5,15 +5,15 @@ import java.util.Scanner;
 
 public class LyricFinder{
 	
-	private static final String[] songNames = new String[] {"7rings", "babyshark", "igottafeeling"};
+	private static final String[] songs = new String[] {"7rings", "babyshark", "igottafeeling"};
 	
 	
 	
 	public static void main(String[] args) {
-	Scanner keyboard = new Scanner(System.in);
+		Scanner keyboard = new Scanner(System.in);
         String userInput;
         
-	String lyricLine1;
+        String lyricLine1;
         String lyricLine2;
         
         System.out.println("\nEnter a lyric and we'll do our best to find the song: ");
@@ -21,7 +21,7 @@ public class LyricFinder{
         
         while (userInput != null) {
         	if (userInput.toLowerCase().equals(lyricLine1) || userInput.toLowerCase().equals(lyricLine2)) {
-        		System.out.println(Song);
+        		System.out.println();
         	}
             
         }
@@ -32,18 +32,18 @@ public class LyricFinder{
 	
 	
 	private static Song[] getSongs() throws FileNotFoundException{
-        Song[] songs = new Song[songNames.length];
-        for (int i = 0; i <= songNames.length - 1; i++) {
-               String songName = songNames[i];
+        Song[] song = new Song[songs.length];
+        for (int i = 0; i <= songs.length - 1; i++) {
+               String songName = songs[i];
                
                File songFile = new File("Songs/" + songNames[i]);
                if (songFile.exists()) {
                      ArrayList<String> contents = getContents(songFile);
                      
-                     songs[i] = new Song(teamName, teamCity, teamStats);
+                     songs[i] = new Song(lyrics, songTitle, artist);
                }
         }
-        return songs;
+        return song;
   }
 	
 	
