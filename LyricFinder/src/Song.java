e3e4e155187436aaa6e0fc4e6ef2d64f86e89713
anthonypeters <anthony.peters68@yahoo.com>
2019-04-10 20:38:21 -0500

    Update Song.java

--- LyricFinder/src/Song.java
+++ LyricFinder/src/Song.java
@@ -2,17 +2,19 @@ import java.io.FileNotFoundException;
 
 public class Song {
 	private String lyrics;
-	private String songTitle;
+	private String songName;
 	private String artist;
 	
-	public Song(String lyrics, String songTitle, String artist) throws FileNotFoundException {
+	public Song(String songName, String artist, String lyrics) throws FileNotFoundException {
         this.lyrics = lyrics;
-        this.songTitle = songTitle;
+        this.songName = songName;
         this.artist = artist;
   }
 	public String toString() {
-        return String.format("%s %s: %s", lyrics, songTitle, artist);
+        return String.format("%s %s: %s", songName, artist, lyrics);
   }
-	
+	public String getSongName() {
+        return this.songName;
+  }
 
 }
